f4fb4df4052dd57872d6aa4c1c33c10035806807
Kiran1178 <kiran_b_kakde@yahoo.co.in>
2019-01-28 17:56:30 +0530

    Class 01 + class 02

---
+++ Class02.py
@@ -0,0 +1,36 @@
+print("Hello world")
+
+
+# ### Integer
+integer_variable = 1
+print(integer_variable)
+# print('Integer')
+# print(integer_variable, type(integer_variable), id(integer_variable))
+float_variable = 3.4
+print(float_variable)
+# ### String
+string_variable_5 = """
+This is my Test multi liner string. 
+Can write anything for as a TEST code.
+Anyone can refer this TEST code
+If any changes are needed then need to inform
+then commit. 
+"""
+print(string_variable_5)
+# ### Boolean
+True_Python = True
+False_Python = False
+print(True_Python, type(True_Python), id(True_Python))
+print(False_Python, type(False_Python), id(False_Python))
+
+# ### None
+None_variable = None
+print(None_variable, type(None_variable), id(None_variable))
+
+# ### List
+list_variable =['Kiran Kakde', 'home address', '7760675006']
+print(list_variable)
+
+
+
+
