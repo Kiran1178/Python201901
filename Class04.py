a3d455e41767bc7d9da93ac30494f5aab06ec44f
Kiran1178 <kiran_b_kakde@yahoo.co.in>
2019-02-03 11:06:55 +0530

    Class04

---
+++ Class04.py
@@ -0,0 +1,32 @@
+"""
+if <Condition>:
+    statement
+
+"""
+number_01 =4
+number_02 =5
+number_03 =6
+
+if number_01 < number_02:
+    print('Number 02 is greater than Number01.')
+
+#if else
+
+if number_01 > number_02:
+    print("Number 01 is greater than Number02.")
+else:
+    print('Number 02 is greater than Number01.')
+
+#if elif else
+
+if number_01 > number_02:
+    print("Number 02 is greater than Number 01.")
+
+elif number_02 < number_03:
+    print('Number 03 is greater than Number02.')
+
+else:
+    print('Number 02 is greater than Number01.')
+
+
+
