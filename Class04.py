e18aa052d176ee22dd0cc596449157a9876ca3c7
Kiran1178 <kiran_b_kakde@yahoo.co.in>
2019-02-03 13:12:40 +0530

    Class04

--- Class04.py
+++ Class04.py
@@ -146,6 +146,14 @@ for i in range(0, 5):
     for j in range(0, 5):
         print("1", end=" ")
     print('')
+#
+
+for i in range(0, 5):
+
+    for j in range(0, 5):
+        print("*", end=" ")
+    print('')
+
 
 
 
