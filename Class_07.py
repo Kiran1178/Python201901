cc0bcddf313024a3da060516bcafff5d5ac8df6f
Kiran1178 <kiran_b_kakde@yahoo.co.in>
2019-03-02 10:43:56 +0530

    class 07

---
+++ Class_07.py
@@ -0,0 +1,53 @@
+# # # #############
+# #
+# # def is_prime(number):
+# #     """check number is prime or not"""
+# #
+# #
+#
+# def prime_recursive(start, limit):
+#     if start == limit:
+#         return
+#
+#     flag = True
+#     for divider in range(2, start):
+#         if start % divider == 0:
+#             flag = False
+#             break
+#
+#     if flag:
+#          print(F'{start} is prime.')
+#
+#     prime_recursive(start +1, limit)
+#
+# prime_recursive(2, 15)
+# #
+# # ###
+#
+#
+# _list = [1, 2, 3, 5, 8, 6, 19,10, 2, 3, 45]
+# print(min(_list))
+# print(max(_list))
+#
+# print(_list.count(1))
+# print(len(_list))
+#
+# print("this is what you want to split".split(" "))
+#
+# field = ['name', 'number']
+# value = ['kiran', '8087004247']
+#
+# for field, value in zip(field, value):
+#     print(field, value)
+#
+# __list = [1, "a", ['test'],{'test3': 'test4'}]
+#
+# for i in __list:
+#     print(type(i), i)
+#
+# print(sum(_list))
+#
+# print(_list)
+# __list.sort()
+# print(__list)
+
